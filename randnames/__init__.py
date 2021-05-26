84728ea8ba9b64083f36f9c9ee349be94df31186
Adam Walkiewicz <aj.walkiewicz@gmail.com>
2021-05-26 14:32:12 +0200

    cleaning up after renaming from randnames to randname

--- randnames/__init__.py
+++
@@ -1,8 +0,0 @@
-# __init__.py
-from randnames.randnames import *
-
-__title__ = 'randnames'
-__version__ = "0.1.1"
-__author__ = 'Adam Walkiewicz'
-__license__ = 'MIT'
-
--- randnames/__main__.py
+++
@@ -1,8 +0,0 @@
-# __main__.py
-from randnames import randnames
-
-def main():
-    return f"{randnames.first_name()} {randnames.last_name()}"
-
-if __name__ == "__main__":
-    print(main())
